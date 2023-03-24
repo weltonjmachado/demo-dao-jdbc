6dce1debbd1a616d8dd46ef15a7d5a31465418a9
Welton Johni Machado <welton.jmachado@outlook.com>
2023-03-24 18:46:58 -0300

    findById implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,5 +1,40 @@
 package model.dao.impl;
 
-public class DepartmentDaoJDBC {
+import java.util.List;
+
+import model.dao.DepartamentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartamentDao {
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
